8a25be54b692de9926b656bcf4e7203a257b22c7
jbelle <jordie.r.belle@gmail.com>
2023-04-26 12:52:20 +0000

    minor formatting and comment improvements, corrected typo in api key reading

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,21 +1,38 @@
-import streamlit as st
-import openai
 import os
+import time
 import tempfile
-from pytube import YouTube
 from typing import Tuple
 
+import openai
+import streamlit as st
+from pytube import YouTube
+
 # Set your OpenAI API key
-openai.api_key = os.enviorn['OPENAI_API_KEY']
+openai.api_key = os.environ['OPENAI_API_KEY']
+
 
 def download_youtube_video(url: str, resolution: str = 'lowest') -> str:
+    """
+    Download a YouTube video in MP4 format.
+
+    :param url: The YouTube video URL.
+    :param resolution: The desired video resolution (default is 'lowest').
+    :return: The local file path of the downloaded video.
+    """
     yt = YouTube(url)
     stream = yt.streams.filter(file_extension='mp4', res=resolution).first()
     output_path = os.path.join(tempfile.gettempdir(), f"{yt.title}.mp4")
     stream.download(output_path=output_path)
     return output_path
 
+
 def transcribe_video(video_path: str) -> str:
+    """
+    Transcribe a video using OpenAI's Whisper ASR model.
+
+    :param video_path: The local file path of the video.
+    :return: The transcription text.
+    """
     # Upload the video file to OpenAI's servers
     with open(video_path, "rb") as f:
         file = openai.File.create(file=f.read(), purpose="transcription")
@@ -37,7 +54,15 @@ def transcribe_video(video_path: str) -> str:
 
     return transcript.text
 
+
 def summarize_text(text: str, length: int = 100) -> str:
+    """
+    Summarize a given text using OpenAI's text-davinci-003 model.
+
+    :param text: The input text to be summarized.
+    :param length: The desired length of the summary in words (default is 100).
+    :return: The summary text.
+    """
     prompt = f"Please summarize the following text in about {length} words: {text}"
     response = openai.Completion.create(
         engine="text-davinci-003",
@@ -52,13 +77,12 @@ def summarize_text(text: str, length: int = 100) -> str:
 
     return summary
 
-# Functions from the previous code
 
 def main():
     st.title("YouTube Video Transcription and Summarization")
 
     youtube_url = st.text_input("Enter the YouTube video URL:")
-    
+
     if youtube_url:
         video_path = download_youtube_video(youtube_url)
 
@@ -74,10 +98,11 @@ def main():
             summary_length = st.slider("Enter the desired summary length (in words):", 10, 500, 100)
 
             st.write("Summarizing the transcription...")
-            summary = summarize_text(transcription, length=summary_length*4)
+            summary = summarize_text(transcription, length=summary_length * 4)
 
             st.write("Summary:")
             st.write(summary)
+
 
 if __name__ == "__main__":
     main()
